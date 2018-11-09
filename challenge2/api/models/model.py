0b0e261e6049e5fe9c383b053fd3f512340de565
BagzieGracious <bagendadeogracious@gmail.com>
2018-11-09 20:05:01 +0300

    add model file [Delivers #161814331, #161815398, #161815084, #161814645]

---
+++ challenge2/api/models/model.py
@@ -0,0 +1,49 @@
+"""
+Module that acts a model, for handling data manipulation
+"""
+from flask import jsonify
+
+class Model:
+    """
+    model class that add data to data structures
+    """
+    lsts = []
+    userLst = []
+
+    def get_order(self, order_id=None):
+        """
+        method for get data from data structures
+        """
+        
+        if order_id is None:
+            return jsonify({"success":True, "data":Model.lsts}), 200
+        for order in Model.lsts:
+            if order.get('order_id') == order_id:
+                return jsonify({"success":True, "data":order}), 200
+        return jsonify({"success":False, "error":{"message": "order not found"}}), 404
+
+    def get_order_user(self, user_id):
+        for order in Model.lsts:
+            if order.get('user_id') == user_id:
+                Model.userLst.append(order)
+            else:
+                Model.userLst.clear()
+        
+        if len(Model.userLst) > 0:
+            return jsonify({"success":True, "data":Model.userLst}), 200
+        return jsonify({"success":False, "error":{"message": "order not found"}}), 404
+
+
+    def cancel_order(self, order_id):
+        for order in Model.lsts:
+            if order.get('order_id') == order_id:
+                order['status'] = 'cancelled'
+                return jsonify({"success":True, "data":order}), 200
+        return jsonify({"success":False, "error":{"message": "order not found"}}), 404
+
+    def create_order(self, order):
+        """
+        method for add an item in the data structure
+        """
+        Model.lsts.append(order)
+        return jsonify({"success":True, "data":order}), 201
