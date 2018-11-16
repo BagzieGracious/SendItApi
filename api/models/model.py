0fd26c5ea12066f99e82fc5fbb84fcd0f867f050
BagzieGracious <bagendadeogracious@gmail.com>
2018-11-16 15:26:28 +0300

    update model files

--- api/models/model.py
+++ api/models/model.py
@@ -3,6 +3,7 @@ Module that acts a model, for handling data manipulation
 """
 from flask import jsonify
 
+
 class Model:
     """
     model class that add data to data structures
@@ -14,13 +15,13 @@ class Model:
         """
         method for get data from data structures
         """
-        return jsonify({"success":True, "data":Model.lsts}), 200
+        return jsonify({"status": "success", "data":Model.lsts}), 200
 
     def get_single_order(self, order_id, user_id):
         for order in Model.lsts:
             if str(order['order_id']) == order_id and order['user_id'] == user_id:
-                return jsonify({"success": True, "data": order}), 200
-        return jsonify({"success": False, "error": {"message": "order not found"}}), 404
+                return jsonify({"status": "success", "data": order}), 200
+        return jsonify({"status": "failure", "error": {"message": "order not found"}}), 404
 
     def get_order_user(self, usr, user_id):
         """
@@ -34,8 +35,8 @@ class Model:
                     Model.userLst.clear()
 
             if len(Model.userLst) > 0:
-                return jsonify({"success":True, "data":Model.userLst}), 200
-        return jsonify({"success":False, "error":{"message": "order not found"}}), 404
+                return jsonify({"status": "success", "data":Model.userLst}), 200
+        return jsonify({"status": "failure", "error":{"message": "order not found"}}), 404
 
 
     def cancel_order(self, order_id, user_id):
@@ -46,18 +47,18 @@ class Model:
             if order['user_id'] == user_id:
                 if str(order['order_id']) == order_id:
                     order['status'] = 'cancelled'
-                    return jsonify({"success":True, "data":order}), 200
-        return jsonify({"success":False, "error":{"message": "order not found"}}), 404
+                    return jsonify({"status": "success", "data":order}), 200
+        return jsonify({"status": "failure", "error": {"message": "order not found"}}), 404
 
     def create_order(self, order):
         """
         method for add an order in the data structure
         """
         Model.lsts.append(order)
-        return jsonify({"success":True, "data":order}), 201
+        return jsonify({"status": "success", "data":order}), 201
 
-    def check_for_exist(self,user_id, product, destination ):
+    def check_for_exist(self, user_id, product, destination ):
         for order in Model.lsts:
             if order['user_id'] == user_id and order['product'] == product and order['destination'] == destination:
-                return jsonify({"success": False, "error": {"message": "you have created that order previously"}}), 400
+                return jsonify({"status": "failure", "error": {"message": "you have created that order previously"}}), 400
         return False
